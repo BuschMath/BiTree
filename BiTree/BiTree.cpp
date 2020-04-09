70de0dece205c344df66ed3f33ce350b9b725822
BuschMath <BuschMath@users.noreply.github.com>
2020-04-09 15:58:26 -0500

    Insert and delete items methods added.

--- BiTree/BiTree.cpp
+++ BiTree/BiTree.cpp
@@ -112,6 +112,7 @@ void BiTree::PutItem(ItemType item)
 
 void BiTree::DeleteItem(ItemType item)
 {
+	Delete(root, item);
 }
 
 void BiTree::ResetTree()
@@ -123,8 +124,13 @@ ItemType BiTree::GetNextItem(bool& finished)
 	return ItemType();
 }
 
-void BiTree::Print(ofstream outfile)
+void BiTree::Print(ofstream &outfile)
 {
+}
+
+void BiTree::BPutItem(ItemType item)
+{
+	BInsert(root, item);
 }
 
 LeftOrRight BiTree::Evaluation(ItemType item, ItemType compare)
@@ -135,4 +141,69 @@ LeftOrRight BiTree::Evaluation(ItemType item, ItemType compare)
 		return Greater;
 
 	return Equal;
+}
+
+void BiTree::BInsert(TreeNode* node, ItemType item)
+{
+	if (node == nullptr)
+	{
+		node = new TreeNode;
+		node->left = nullptr;
+		node->right = nullptr;
+		node->info = item;
+	}
+	else if (item < node->info)
+		BInsert(node->left, item);
+	else
+		BInsert(node->right, item);
+}
+
+void BiTree::Delete(TreeNode* node, ItemType item)
+{
+	if (item < node->info)
+		Delete(node->left, item);
+	else if (item > node->info)
+		Delete(node->right, item);
+	else
+	{
+		DeleteNode(node);
+	}
+}
+
+void BiTree::DeleteNode(TreeNode* node)
+{
+	TreeNode* temp = node;
+	TreeNode* parent;
+	if (node->left == nullptr && node->right == nullptr)
+	{
+		delete node;
+	}
+	else if (node->left != nullptr && node->right == nullptr)
+	{
+		node->info = node->left->info;
+		temp = node->left;
+		node->right = node->left->right;
+		node->left = node->left->left;
+		delete temp;
+	}
+	else if (node->left == nullptr && node->right != nullptr)
+	{
+		node->info = node->right->info;
+		temp = node->right;
+		node->left = node->right->left;
+		node->right = node->right->right;
+		delete temp;
+	}
+	else
+	{
+		temp = node->right;
+		while (temp->left != nullptr)
+		{
+			parent = temp;
+			temp = temp->left;
+		}
+		parent->left = temp->right;
+		node->info = temp->info;
+		delete temp;
+	}
 }
--- BiTree/BiTree.h
+++ BiTree/BiTree.h
@@ -36,12 +36,18 @@ public:
 	void DeleteItem(ItemType item);
 	void ResetTree();
 	ItemType GetNextItem(bool& finished);
-	void Print(ofstream outfile);
+	void Print(ofstream &outfile);
+
+	void BPutItem(ItemType item);
 
 private:
 	TreeNode* root;
 
 	LeftOrRight Evaluation(ItemType item, ItemType compare);
+
+	void BInsert(TreeNode* node, ItemType item);
+	void Delete(TreeNode* node, ItemType item);
+	void DeleteNode(TreeNode* node);
 };
 
 #endif // !BITREE_H
